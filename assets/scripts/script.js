e8cd4593b9de9521098c45f29aee18f51b7608d7
arnuga3 <arniszelcs@gmail.com>
2017-01-31 23:03:19 +0000

    image modal corerections

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,29 @@
 $(document).on('ready', function() {
+
+
+	// PHOTOS MODAL
+	var modal = $('#myModal');
+	var modalImg = $('#img01');
+	var captionText = $('#caption');
+	$('.myImg').each(function() {
+			$(this).on('click', function() {
+					modal.css({'display':'block'});
+					modalImg.attr('src', $(this).attr('src'));
+					captionText.text($(this).attr('alt'));
+			});
+	});
+	$('#closeModal').on('click', function() {
+			modal.css({'display':'none'});
+	});
+
+	modal.on('click', function() {
+		if (modalImg.css('display') == 'block') {
+				modal.css({'display':'none'});
+				console.log("hi");
+		}
+	})
+
+	// Side Menu
 	$('#menuBtn').on('click', function() {
 		if ($('#sideMenu').css('right') == '-250px') {
 			// move all elements to the left - open
@@ -18,21 +43,6 @@ $(document).on('ready', function() {
 			$(this).animate({right: '0px'}, 50);
 			$('#menuBtn').children().removeClass("change");		// return to normal menu icon
 		}
-	});
-
-	// PHOTOS MODAL
-	var modal = $('#myModal');
-	var modalImg = $('#img01');
-	var captionText = $('#caption');
-	$('.myImg').each(function() {
-			$(this).on('click', function() {
-					modal.css({'display':'block'});
-					modalImg.attr('src', $(this).attr('src'));
-					captionText.text($(this).attr('alt'));
-			});
-	});
-	$('#closeModal').on('click', function() {
-			modal.css({'display':'none'});
 	});
 
 });
